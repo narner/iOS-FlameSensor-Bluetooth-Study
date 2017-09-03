51e7deeca9acb94da58b46809e148385d7676a17
Nicholas Arner <nicholasarner@gmail.com>
2017-09-03 09:13:12 -0400

    Initial commit

---
+++ bleuart_cmdmode/bleuart_cmdmode.ino
@@ -0,0 +1,147 @@
+/*********************************************************************
+ This is an example for our nRF51822 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include <CapacitiveSensor.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+#define BLUEFRUIT_HWSERIAL_NAME Serial1
+
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+/* Create Bluefruit object using hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+
+CapacitiveSensor   cs_9_10 = CapacitiveSensor(9,10);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+// lowest and highest sensor readings:
+const int sensorMin = 0;     // sensor minimum
+const int sensorMax = 1024;  // sensor maximum
+
+void setup(void) {
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+  cs_9_10.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+
+  Serial.begin(115200);
+
+  if ( !ble.begin(VERBOSE_MODE) ) {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE ) {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) ) {
+    // Change Mode LED Activity
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+    Serial.println(F("******************************"));
+  }
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void) {
+
+    long total1 =  cs_9_10.capacitiveSensor(30);
+    // read the sensor on analog A0:
+    int sensorReading = analogRead(A0);
+    // map the sensor range (four options):
+    // ex: 'long int map(long int, long int, long int, long int, long int)'
+    int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+
+    // range value:
+    switch (range) {
+        case 0:    // A fire closer than 1.5 feet away.
+          Serial.print(0);
+          Serial.flush();
+          delay(20);
+          //Serial.print("** Close Fire **");
+        break;
+        case 1:    // A fire between 1-3 feet away.
+          Serial.print(1);
+          Serial.flush();
+          delay(20);
+          //Serial.print("** Distant Fire **");
+         break;
+        case 2:    // No fire detected.
+          Serial.print(2);
+          Serial.flush();
+          delay(20);
+          //Serial.print("No Fire");
+        break;
+      }
+
+
+    ble.print("AT+BLEUARTTX=");
+    ble.println(total1);
+
+    // check response stastus
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+}
+
